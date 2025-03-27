20803c68abc3f13cd269955626498f1bbfb92a67
Jitesh Bhakat <137819786+Jitesh8260@users.noreply.github.com>
2025-03-27 20:33:37 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+  content: [
+    "./public/index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  safelist: [
+    "bg-gray-900",
+    "text-white",
+    "p-4",
+    "shadow-md",
+    "text-yellow-400",
+    "hover:text-yellow-400"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
